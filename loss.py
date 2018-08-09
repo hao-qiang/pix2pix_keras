dbe9d439caa521877cde6ad8ffb2158c3d1d5d89
Hao Qiang <wind209@sina.com>
2018-08-09 16:39:39 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.applications.vgg19 import VGG19
 import keras.backend as K
 
@@ -27,7 +27,9 @@ def perceptual_loss(y_true, y_pred):
 
 
 def l1_perceptual_loss(y_true, y_pred):
-    vgg = VGG19(include_top=False, weights='imagenet', input_shape=(256,256,3))
-    loss_model = Model(inputs=vgg.input, outputs=vgg.get_layer('block5_conv4').output)
+    y_true = img_preprocess(y_true)
+    y_pred = img_preprocess(y_pred)
+    vgg = VGG16(include_top=False, weights='imagenet', input_shape=(256,256,3))
+    loss_model = Model(inputs=vgg.input, outputs=vgg.get_layer('block3_conv3').output)
     loss_model.trainable = False
-    return 0.006*K.mean(K.square(loss_model(y_true) - loss_model(y_pred)))+K.mean(K.abs(y_pred - y_true), axis=-1)
+    return 0.006 * K.mean(K.square(loss_model(y_true) - loss_model(y_pred))) + K.mean(K.abs(y_pred - y_true), axis=-1)
